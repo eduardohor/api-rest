24500852262e800f5ed2a1d646d1cc2cbca1336a
eduardohor <eduardo.hor@outlook.com>
2022-08-05 12:28:24 -0300

    adding models and relationships

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Store::factory(10)
+            ->hasProducts(10)
+            ->create();
     }
 }
